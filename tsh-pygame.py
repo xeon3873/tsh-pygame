054914ec255ec8421956fc3c558eca261c657351
xeon3873 <xeon.ahs@gmail.com>
2024-10-20 13:11:43 +0900

    Auto Commit

--- tsh-pygame.py
+++ tsh-pygame.py
@@ -11,6 +11,8 @@ pygame.display.set_caption("TSH Pygame")
 background = pygame.image.load("배경.jpg")
 background = pygame.transform.scale(background, (screen_width, screen_height))
 
+stage_height = 400
+
 character = pygame.image.load("character/캐릭터-총X버전-removebg-preview.png")
 character = pygame.transform.scale(character, (150, 150))
 character_size = character.get_rect().size
@@ -99,6 +101,49 @@ while running:
         ball_width = ball_size[0]
         ball_height = ball_size[1]
 
+        # 가로벽에 닿았을 때 공 이동 위치 변경 (튕겨 나오는 효과)
+        if ball_pos_x < 0 or ball_pos_x > screen_width - ball_width:
+            ball_val["to_x"] = ball_val["to_x"] * -1
+
+        if ball_pos_y >= screen_height - stage_height - ball_height:
+            ball_val["to_y"] = ball_val["init_spd_y"]
+        else:
+            ball_val["to_y"] += 0.5
+
+        ball_val["pos_x"] += ball_val["to_x"]
+        ball_val["pos_y"] += ball_val["to_y"]
+
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        ball_rect = ball_images[ball_img_idx].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+
+        if character_rect.colliderect(ball_rect):
+            running = False
+            break
+
+        for weapon_idx, weapon_val in enumerate(weapons):
+            weapon_pos_x = weapon_val[0]
+            weapon_pos_y = weapon_val[1]
+
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_idx
+                ball_to_remove = ball_idx
+
+                if ball_img_idx < 3:
+                    ball_width = ball_rect
         
 
     screen.blit(background, (0, 0))
